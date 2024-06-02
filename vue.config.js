e5acbb8571cbfff5289f336a578718ddb4fc2b8b
ayush <ayushkr347@gmail.com>
2024-06-02 11:35:59 +0530

    repo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-   publicPath: process.env.NODE_ENV === 'production' ? '/<indee-project>/' : '/',
-  transpileDependencies: true
+mport { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+export default defineConfig({
+  base: '/indee-project/', // Replace with your GitHub repository name
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
